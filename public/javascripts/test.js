768f5bd1ad5447c6653f41451d79038fb73464b4
Sam Genduso <sam.genduso@gmail.com>
2015-08-11 15:31:02 -0600

    login and signup form setup

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,16 +1,11 @@
 var notLoggedIn = document.getElementById('notLoggedIn');
-var loginModal = document.getElementById('loginModal')
+var loginModal = document.getElementById('loginModal');
+var closeModal = document.getElementById('closeModal');
 
 notLoggedIn.addEventListener('click', function () {
-  loginModal.innerHTML = "";
-  var text = document.createElement('p');
-  var close = document.createElement('button');
-  close.innerHTML = 'X';
-  loginModal.appendChild(text).innerHTML = "test modal";
-  loginModal.appendChild(close);
   loginModal.showModal();
+});
 
-  close.addEventListener('click', function () {
-    loginModal.close();
-  })
+closeModal.addEventListener('click', function () {
+  loginModal.close();
 });
