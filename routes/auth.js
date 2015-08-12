da6ebc56a62707b22b6ecb3aff79910674a4b604
Claire Nessler <clrnessler@gmail.com>
2015-08-12 16:51:19 -0600

    mongo queries

--- routes/auth.js
+++ routes/auth.js
@@ -17,24 +17,13 @@ router.get('/jobs', function(req, res, next) {
 
 router.post('/signup', function (req, res, next) {
   var user = req.body;
-  db.User.findOne({email: user.signup_email})
-  .then(function (foundUser) {
-    if(!foundUser) {
-      user.signup_password = bcrypt.hashSync(user.signup_password, 8);
-      db.User.create(
-        {
-          firstName: user.signup_first_name,
-          lastName: user.signup_last_name,
-          email: user.signup_email,
-          password: user.signup_password,
-          jobsPosted: []
-        })
-      .then(function (newUser) {
+    databaseQueries.signUp(user).then(function (newUser) {
+      if(newUser){
         req.session.userId = newUser._id;
         req.session.userFirstName = newUser.firstName;
         res.redirect('/newjob');
-      });
-    } else {
+      }
+     else {
       res.render('jobs', { title: 'JOB BOARD', error: 'This email is already associated with an account.'});
     }
   });
