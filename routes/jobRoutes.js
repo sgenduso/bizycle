89a0e89b7dafe4594263b43b36f938cdc7f4d549
Amanda Carlton <acarlton27@gmail.com>
2015-08-12 16:29:59 -0600

    newjob db query

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,39 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var validate = require('../lib/validate.js')
-
-
+var validate = require('../lib/validate.js');
 var db = require('../models');
+var databaseQueries = require('../lib/database.js');
 
 router.get('/newjob', function (req, res, next) {
   res.render('job/newjob');
 });
 
 router.post('/newjob', function (req, res, next) {
-  var errorCheck = validate.validate(req.body.title, req.body.company, req.body.location, req.body.description, req.body.expiry)
+  var errorCheck = validate.validate(req.body.title, req.body.company, req.body.location, req.body.description, req.body.expiry);
   if(errorCheck.length > 0){
-    res.render('newjob', {errors: errorCheck})
+    res.render('newjob', {errors: errorCheck});
   } else {
-  db.Job.create(
-    {
-      jobTitle: req.body.title,
-      companyName: req.body.company,
-      location: req.body.location,
-      activationDate: req.body.activate,
-      expiryDate: req.body.expiry,
-      contactInfo: req.body.contact,
-      description: req.body.description,
-      requirements: req.body.requirements,
-      source: req.body.source
-    },
-    function(err, job){
-      if(err){
-        // console.log(err);
-      } else {
-        // console.log(job);
-      }
-    }
-  ).then(function () {
+    databaseQueries.newJobs(req.body)
+  .then(function () {
     res.redirect('/jobs');
   });
   }
