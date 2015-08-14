f35e4863703797ad8d85f12dc2eea146de69d0bc
Akhil Sankar <akhil.sankar@gmail.com>
2015-08-14 10:43:18 -0600

    CRUD: update posts, delete posts, delete user(and associated posts)

--- routes/profile.js
+++ routes/profile.js
@@ -9,17 +9,37 @@ router.get('/profile', function (req, res, next) {
   var userCookie = req.session.userId;
   databaseQueries.populateProfile(userCookie)
   .then(function (postedJobs) {
-    console.log(postedJobs, "POSTED");
     res.render('profile', {jobs: postedJobs})
   })
 })
 
 router.get('/job/:id/edit', function (req, res, next) {
   databaseQueries.findOneJob(req.params.id).then(function (job) {
-    console.log(job.activationDate, "activation date");
-    console.log(job, "THIS IS A JOB");
     res.render('job/edit', {job: job})
 
+  })
+})
+
+router.post('/job/:id/edit', function (req, res, next) {
+  databaseQueries.updateJob(req.params.id, req.body)
+  .then(function () {
+    res.redirect('/profile')
+  });
+})
+
+router.get('/job/:id/delete', function (req, res, next) {
+  databaseQueries.deleteJob(req.params.id)
+  .then(function () {
+    console.log("deleted sucessfully");
+    res.redirect('/profile');
+  })
+})
+
+router.get('/profile/:id/delete', function (req, res, next) {
+  databaseQueries.deleteUser(req.params.id)
+  .then(function () {
+    req.session = null;
+    res.redirect('/');
   })
 })
 
