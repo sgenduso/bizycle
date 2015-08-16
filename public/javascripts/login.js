431a8c4db663f6ed6b39149eeba8ac5c35176c40
Akhil Sankar <akhil.sankar@gmail.com>
2015-08-16 08:35:04 -0600

    login modal works for taskbar and post job buttons

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -9,36 +9,32 @@ var hiddenInput = document.createElement('input');
 var hiddenInputOne = document.createElement('input');
 var appendDivs = document.getElementsByClassName('append');
 
-console.log(notLoggedInElements[0].id);
-
 if (errors) {
   loginModal.showModal();
 }
 
 if(notLoggedInElements) {
-  for (var i = 0; i < 2; i++) {
-    function (notLoggedInElements[i]) {
-      console.log(notLoggedInElements[i]);
-      notLoggedInElements[i].addEventListener('click', function () {
+  for (var i = 0; i < notLoggedInElements.length; i++) {
+    console.log(notLoggedInElements[i]);
+    notLoggedInElements[i].addEventListener('click', function () {
       hiddenInput.type='text';
       hiddenInput.style.display='none'
-      hiddenInput.value=notLoggedInElements[i].id;
+      hiddenInput.value=this.id;
       hiddenInput.name='path';
 
       hiddenInputOne.type='text';
       hiddenInputOne.style.display='none'
-      hiddenInputOne.value=notLoggedInElements[i].id;
+      hiddenInputOne.value=this.id;
       hiddenInputOne.name='path';
 
       appendDivs[0].appendChild(hiddenInputOne);
       appendDivs[1].appendChild(hiddenInput);
 
       loginModal.showModal();
-      })
-    }(notLoggedInElements[i]);
+    })
+    closeModal.addEventListener('click', function () {
+      loginModal.close();
+    });
   }
-}
 
-  closeModal.addEventListener('click', function () {
-    loginModal.close();
-  });
+}
